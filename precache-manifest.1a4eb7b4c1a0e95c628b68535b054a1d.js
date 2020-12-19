c839524e1dd2a09f7a772ef3b8fcb89a8d2cb5ba
Malyhin Ivan <malygin91@gmail.com>
2020-12-19 13:47:48 +0200

    Updates

---
+++ precache-manifest.1a4eb7b4c1a0e95c628b68535b054a1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3bc03355b13b89520312b6318957cc",
+    "url": "/game_cards/index.html"
+  },
+  {
+    "revision": "8e4399ea25ab1cd55148",
+    "url": "/game_cards/static/css/main.77a1ad0f.chunk.css"
+  },
+  {
+    "revision": "7d85735d5c92f99e3fdc",
+    "url": "/game_cards/static/js/2.1f4ff41e.chunk.js"
+  },
+  {
+    "revision": "28f29a598e465553cfeea810767399d3",
+    "url": "/game_cards/static/js/2.1f4ff41e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4399ea25ab1cd55148",
+    "url": "/game_cards/static/js/main.5bd0c3fa.chunk.js"
+  },
+  {
+    "revision": "16c6933be7f9808aca2a",
+    "url": "/game_cards/static/js/runtime-main.90388a68.js"
+  }
+]);
